65c55fb26c9c0c656e1e06d605bcdfae052e9af0
alihamzase <76881652+alihamzase@users.noreply.github.com>
2021-01-09 23:21:48 +0500

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,6 @@
+var message= "karachi";
+var message1= "KARACHI";
+message= message2.toUpperCase()
+if (message == message1){
+    alert("Match")
+}
---
+++ task4.js
@@ -0,0 +1,6 @@
+var text = "How bright they've grown in the sunlight!";
+for(var i = 0; i < text.length; i++){
+    if(text.charAt(i) === "!"){
+        alert("Found");
+    }
+}
